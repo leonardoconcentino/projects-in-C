ab4e8564c65be3266af10c29c9aba4b23b4b5a74
Leonardo Concentino <llconcentino@gmail.com>
2025-01-28 17:15:22 -0300

    My first "Hello World!" with C

--- helloworld.c
+++ helloworld.c
@@ -1,7 +1,9 @@
-#include <stdio.h> //inserir a biblioteca stdIO
+#include <stdio.h> // includes stdIO library to be able to code in C
 
-int main() { // função para o algoritmo todo
+int main(){ // the main function. all the algorithm goes inside this "{}"
 
-    printf("Olá, Mundo!\n"); // função printf para enviar uma mensagem no prompt para o usuário. "\n" adiciona uma nova linha após a mensagem
+    printf("Hello, World!\n"); // print the message on the terminal to the user
 
-}
+    return 0; // finish the algorithm without any issues
+
+} //closes the main function
