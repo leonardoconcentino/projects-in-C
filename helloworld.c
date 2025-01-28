a6073c5a9ceb3f10214963c07da630ef20496e3c
Leonardo Concentino <llconcentino@gmail.com>
2025-01-28 17:14:07 -0300

    My first Hello World! with C

---
+++ helloworld.c
@@ -0,0 +1,7 @@
+#include <stdio.h> //inserir a biblioteca stdIO
+
+int main() { // função para o algoritmo todo
+
+    printf("Olá, Mundo!\n"); // função printf para enviar uma mensagem no prompt para o usuário. "\n" adiciona uma nova linha após a mensagem
+
+}
